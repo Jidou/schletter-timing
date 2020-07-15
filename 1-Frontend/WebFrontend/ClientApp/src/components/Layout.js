cc5d01dcde4c90602fb8f428165ddfd16f325bbb
florian seidner <florian.seidner@gmail.com>
2020-07-15 16:17:49 +0200

    Adds first draft of dark theme toggler

--- 1-Frontend/WebFrontend/ClientApp/src/components/Layout.js
+++ 1-Frontend/WebFrontend/ClientApp/src/components/Layout.js
@@ -1,34 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Container } from 'reactstrap';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { Button, Navbar, Nav, NavDropdown } from 'react-bootstrap';
+
+import { useDarkMode } from './useDarkMode';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './theme';
+import { GlobalStyles } from './global';
+
 
 export class Layout extends Component {
   static displayName = Layout.name;
 
-  render () {
-    return (
-      <div>
-        <Navbar bg="primary" variant="dark">
-          <Navbar.Brand href="/">Schletter Timing</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-            <Nav className="mr-auto">
-              <Nav.Link href="/races">Races</Nav.Link>
-              <Nav.Link href="/groups">Groups</Nav.Link>
-              <Nav.Link href="/participants">Participants</Nav.Link>
-              <NavDropdown title="Race" id="basic-nav-dropdown">
-                <NavDropdown.Item href="/race/overview">Overview</NavDropdown.Item>
-                <NavDropdown.Item href="/race/groups">Groups</NavDropdown.Item>
-                <NavDropdown.Item href="/race/participants">Participants</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+    // constructor () {
+    //   const [theme, toggleTheme, componentMounted] = useDarkMode();
+    //   const themeMode = theme === 'light' ? lightTheme : darkTheme;
+    // }
 
-        <Container>
-          {this.props.children}
-        </Container>
-      </div>
-    );
+    render () {
+      return (
+        <div>
+          <Navbar bg="primary" variant="dark">
+            <Navbar.Brand href="/">Schletter Timing</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+              <Nav className="mr-auto">
+                <Nav.Link href="/races">Races</Nav.Link>
+                <Nav.Link href="/groups">Groups</Nav.Link>
+                <Nav.Link href="/participants">Participants</Nav.Link>
+                <NavDropdown title="Race" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="/race/overview">Overview</NavDropdown.Item>
+                  <NavDropdown.Item href="/race/groups">Groups</NavDropdown.Item>
+                  <NavDropdown.Item href="/race/participants">Participants</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              {/* <ThemeProvider theme={lightTheme}>
+                <Button variant="outline-info" onClick={this.toggleTheme}>Toggle theme</Button>
+              </ThemeProvider> */}
+            </Navbar.Collapse>
+          </Navbar>
+
+
+          <Container>
+            {this.props.children}
+          </Container>
+        </div>
+      );
   }
 }
